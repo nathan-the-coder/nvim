4c2c3fe71cc996fd18243e228719be1bc8e4c919
nathan-the-coder <nathanielramos726@gmail.com>
2025-05-12 15:13:11 +0800

    fix: removed  call for mason_lspconfig as of the breaking changes that happened with some refactoring to the colorscheme

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -5,15 +5,15 @@ local mason_lspconfig = require('mason-lspconfig')
 mason.setup({})
 
 mason_lspconfig.setup({
-  ensure_installed = {}, -- You can specify servers here if desired
-  automatic_installation = true,
+  automatic_enable = true,
+  ensure_installed = { "lua_ls" }, -- You can specify servers here if desired
 })
 
-mason_lspconfig.setup_handlers({
-  -- Default handler for all LSP servers
-  function(server_name)
-    require('lspconfig')[server_name].setup({
-      capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    })
-  end,
-})
+-- mason_lspconfig.setup_handlers({
+--   -- Default handler for all LSP servers
+--   function(server_name)
+--     require('lspconfig')[server_name].setup({
+--       capabilities = require('cmp_nvim_lsp').default_capabilities(),
+--     })
+--   end,
+-- })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,14 @@
 
 return {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
-      vim.cmd('colorscheme catppuccin-mocha')
+      local theme_file = io.open("$HOME/.theme", "r")
+      if theme_file then
+        local theme = theme_file:read("a")
+        theme_file:close()
+        vim.cmd("set background="..theme)
+      end
+      vim.cmd[[colorscheme gruvbox]]
     end
 }
